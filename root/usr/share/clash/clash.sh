d0eac3fd579d99fe3113173edc100f0ad83f7205
frainzy1477 <49537471+frainzy1477@users.noreply.github.com>
2019-07-09 21:16:06 +0800

    Update clash.sh

--- root/usr/share/clash/clash.sh
+++ root/usr/share/clash/clash.sh
@@ -2,5 +2,5 @@
 subscribe_url=$(uci get clash.config.subscribe_url 2>/dev/null)
 rm -rf /etc/clash/config.bak 2> /dev/null
 wget-ssl --user-agent="User-Agent: Mozilla" $subscribe_url -O 2>&1 >1 /etc/clash/config.yaml
-[ "$?" -ne "0" ] && wget-ssl -O 2>&1 >1 /etc/clash/config.yml "$subscribe_url"
+[ "$?" -ne "0" ] && wget-ssl -O 2>&1 >1 /etc/clash/config.yaml "$subscribe_url"
 [ "$?" -eq "0" ] && /etc/init.d/clash restart 2>/dev/null
