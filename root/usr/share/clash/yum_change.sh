6293240884769a1efe4e8a26960484efe51235ab
frainzy1477 <allsafe@my.com>
2019-09-08 04:31:04 +0800

    1.0.1b

--- root/usr/share/clash/yum_change.sh
+++ root/usr/share/clash/yum_change.sh
@@ -30,7 +30,8 @@ select_config
 		socks_port=$(uci get clash.config.socks_port 2>/dev/null)
 		dash_port=$(uci get clash.config.dash_port 2>/dev/null)
 		log_level=$(uci get clash.config.level 2>/dev/null)
-		subtype=$(uci get clash.config.subcri 2>/dev/null)		
+		subtype=$(uci get clash.config.subcri 2>/dev/null)
+		lan_ip=$(uci get network.lan.ipaddr 2>/dev/null)		
 if [ $mode -eq 1 ];  then	
 		
 		sed -i "/Proxy:/i\#clash-openwrt" $CONFIG_YAML
@@ -46,7 +47,7 @@ if [ $mode -eq 1 ];  then
 		sed -i "4i\allow-lan: true" $CONFIG_YAML
 		sed -i "5i\mode: Rule" $CONFIG_YAML
 		sed -i "6i\log-level: ${log_level}" $CONFIG_YAML
-		sed -i "7i\external-controller: 0.0.0.0:${dash_port}" $CONFIG_YAML
+		sed -i "7i\external-controller: ${lan_ip}:${dash_port}" $CONFIG_YAML
 		sed -i "8i\secret: '${da_password}'" $CONFIG_YAML
 		sed -i "9i\external-ui: "/usr/share/clash/dashboard"" $CONFIG_YAML
 		sed -i "10i\ " $CONFIG_YAML
@@ -75,7 +76,7 @@ else
 		sed -i "4i\allow-lan: true" $CONFIG_YAML
 		sed -i "5i\mode: Rule" $CONFIG_YAML
 		sed -i "6i\log-level: ${log_level}" $CONFIG_YAML
-		sed -i "7i\external-controller: 0.0.0.0:${dash_port}" $CONFIG_YAML
+		sed -i "7i\external-controller: ${lan_ip}:${dash_port}" $CONFIG_YAML
 		sed -i "8i\secret: '${da_password}'" $CONFIG_YAML
 		sed -i "9i\external-ui: "/usr/share/clash/dashboard"" $CONFIG_YAML
 		sed -i "10i\ " $CONFIG_YAML
