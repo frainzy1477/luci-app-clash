ce7c1a04e47c199312702fb701faff0b9e9a871c
frainzy1477 <allsafee@my.com>
2020-01-16 23:44:31 +0800

    1.4.7d

--- root/usr/share/clash/geoip.sh
+++ root/usr/share/clash/geoip.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-LICENSE_KEY=$(uci get clash.config.license_key 2>/dev/null)
+Key=$(uci get clash.config.license_key 2>/dev/null)
+
 if [ -f /var/run/geoip_down_complete ];then 
   rm -rf /var/run/geoip_down_complete 2>/dev/null
 fi
+
 echo '' >/tmp/geoip_update.txt 2>/dev/null
-wget --no-check-certificate https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key="$LICENSE_KEY"&suffix=tar.gz -O 2>&1 >1 /tmp/GeoLite2-Country.tar.gz
+sleep 3
 
-if [ "$?" -eq "0" ] && [ "$(ls -l /tmp/GeoLite2-Country.tar.gz |awk '{print int($5)}')" -ne 0 ]; then
+wget -c4 --no-check-certificate --timeout=300 --tries=5 --user-agent="Mozilla" https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key="$Key"&suffix=tar.gz -O 2>&1 >1 /tmp/geoip.tar.gz
 
-tar zxvf /tmp/GeoLite2-Country.tar.gz -C .
+if [ "$?" -eq "0" ] && [ "$(ls -l /tmp/geoip.tar.gz |awk '{print int($5)}')" -ne 0 ]; then
+
+tar zxvf /tmp/geoip.tar.gz -C .
 mv /tmp/GeoLite2-Country_*/GeoLite2-Country.mmdb /etc/clash/Country.mmdb
 chmod 755 /etc/clash/Country.mmdb >/dev/null 2>&1
-rm -rf /tmp/GeoLite2-Country_*
+rm -rf /tmp/GeoLite2-Country_* /tmp/geoip.tar.gz
 sleep 2
 touch /var/run/geoip_down_complete >/dev/null 2>&1
 sleep 2
